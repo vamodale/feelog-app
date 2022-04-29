d0b294e61af9c816d9a24fa0c273a463aad25795
ZoTTiz <antonio.ls.zotti@gmail.com>
2022-04-29 16:04:42 -0300

    FL0 - Base do Projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useFonts } from 'expo-font';
+import { Routes } from './src/routes';
+import { Poppins_500Medium, Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { theme } from '../global/styles/theme';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: theme.colors.primary
+                }
+            }}
+        >
+        </Navigator>
+
+    )
+}
