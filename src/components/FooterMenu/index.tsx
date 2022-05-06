1c9abc989a46665b88c90c60af0d832ab2ababd0
mhoffnn <matheushofmar@hotmail.com>
2022-05-06 17:30:49 -0300

    Alteração da foto de perfil e correção no gradient

--- src/components/FooterMenu/index.tsx
+++ src/components/FooterMenu/index.tsx
@@ -29,7 +29,7 @@ export function FooterMenu() {
                 <LinearGradient
                     start={[0.2, 0.2]}
                     end={[0.8, 0.8]}
-                    colors={['#00888D', '#8F009A', '#00C463', '#4DA6FF', '#FFFFEB', '#FFE478', '#FFA34C', '#FF4E41', '#FF4E41',]}
+                    colors={['#00888D', '#00C463', '#4DA6FF', '#FFFFEB', '#FFE478', '#FFA34C', '#FF4E41', '#FF4E41', '#8F009A']}
                     style={{ width: '100%', height: '100%', borderRadius: 34 }}
 
                 >
