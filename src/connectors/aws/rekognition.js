3f517e70cc0a60ea7a8d649284ab7e8f2dc80ee9
Kastango <Metroidling@gmail.com>
2022-06-06 19:56:39 -0300

    change colors at dropdown

--- src/connectors/aws/rekognition.js
+++ src/connectors/aws/rekognition.js
@@ -8,7 +8,7 @@ const rekognitionClient = new RekognitionClient({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  region: process.env.AWS_REGION,
+  region: 'us-east-1',
 });
 
 const EMOTION_ENUM = {
